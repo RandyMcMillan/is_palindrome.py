42a07cc4985e5e4c9ea56aba3da666c4dd3a4ff5
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-05-31 23:14:02 -0400

    is_palindrome.py:

--- is_palindrome.py
+++ is_palindrome.py
@@ -2,11 +2,9 @@
 
 import operator
 
-
 # 𝐴⊕𝐵=𝐴𝐵′+𝐴′𝐵
 global xor
 xor = -1
-
 
 def test():
     # Initializing two integer variables
@@ -37,12 +35,11 @@ def is_palindrome(n):
     s = str(n)
     
     # check if the string is the same forwards and backwards
-    # return s == int(s[::-1])
     return s == s[::-1]
 
 # define the start and end of the range
 start = 0
-end = 21111112
+end = 9999999
 
 # create an empty list to store the palindromes
 palindromes = []
@@ -51,34 +48,21 @@ palindromes = []
 for n in range(start, end+1):
 
     # if the number is a palindrome, add it to the list
-    #  print("n=%d" % n)
     if is_palindrome(n):
 
-        #n = int(n)
-        #print("type(n)=%s",type(n))
+        # print("n=%d" % n)
+        # print("type(n)=%s",type(n))
         palindromes.append(n)
-
-        # print("len(palindromes)=%d" % len(palindromes))
         if len(palindromes) > -1:
 
-            # print("palindromes[ int(len(palindromes)-2) ]=%d" % palindromes[ int(len(palindromes)-2) ])
-            # print("palindromes[ int(len(palindromes)-1) ]=%d" % palindromes[ int(len(palindromes)-1) ])
-
-            # xor = palindromes[ len(palindromes)-1 ] ^ palindromes[len(palindromes)-2]
-            # test()
-            # print("pre:^=%d" % xor)
-            # xor = palindromes[ int(len(palindromes)-1) ] ^ palindromes[ int(len(palindromes)-2) ]
-            # xor = palindromes[ int(len(palindromes)-2) ] ^ palindromes[ int(len(palindromes)-1) ]
             xor = palindromes[ int(len(palindromes)-2) ] ^ palindromes[ int(len(palindromes)-1) ]
-            # print("type(xor)=%s",type(xor))
-            # test()
-            print("%d^" % palindromes[ int(len(palindromes)-2) ]+"%d" % palindromes[ int(len(palindromes)-1) ]+"=%d" % xor)
-            #print("^=%d" % xor)
-            # test()
 
-            # xor = operator.xor(palindromes[ len(palindromes)-2 ], palindromes[len(palindromes)-1])
-            # xor = operator.xor(palindromes[ len(palindromes)-1 ], palindromes[len(palindromes)-2])
-            # print("xor=%d" % xor)
+            # print("type(xor)=%s",type(xor))
+            print("%d^" % palindromes[ int(len(palindromes)-2) ]
+            +"%d" % palindromes[ int(len(palindromes)-1) ]+"=%d" % xor)
+
+            if is_palindrome(xor):
+                palindromes.append(xor)
 
 # print the list of palindromes
-# print(palindromes)
+print(palindromes)
