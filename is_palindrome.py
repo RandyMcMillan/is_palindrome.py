c8699229e9ca7f26ebefb6c87f4c5860f472bacf
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-05-31 21:25:23 -0400

    is_palindrome.py: counting

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python3
 
+import operator
+
+def test():
+    # Initializing two integer variables
+    a = 45
+    b = 21
+    
+    # Printing a and b before swapping
+    print("a before swap =", a)
+    print("b before swap =", b)
+    
+    print()
+    
+    # Swapping a and b using XOR operator
+    a = a ^ b
+    b = a ^ b
+    a = a ^ b
+    
+    # Printing a and b after swapping using XOR
+    print("a after swap =", a)
+    print("b after swap =", b)
+    
+    exit();
+
 # define a function to check if a number is a palindrome
 def is_palindrome(n):
     # convert the number to a string
@@ -9,8 +33,8 @@ def is_palindrome(n):
     return s == s[::-1]
 
 # define the start and end of the range
-start = 1
-end = 1000001
+start = 0
+end = 11
 
 # create an empty list to store the palindromes
 palindromes = []
@@ -18,8 +42,23 @@ palindromes = []
 # loop through the range of numbers
 for n in range(start, end+1):
     # if the number is a palindrome, add it to the list
+    print("n=%d" % n)
     if is_palindrome(n):
+
+        n = int(n)
         palindromes.append(n)
+
+    print("len(palindromes)=%d" % len(palindromes))
+    if len(palindromes) >= 2:
+
+        print("palindromes[ len(palindromes)-2 ]=%d" % palindromes[ len(palindromes)-2 ])
+        print("palindromes[ len(palindromes)-1 ]=%d" % palindromes[ len(palindromes)-1 ])
+
+        xor = palindromes[ len(palindromes)-2 ] ^ palindromes[len(palindromes)-1]
+        print("^=%d" % xor)
+
+        xor = operator.xor(palindromes[ len(palindromes)-2 ], palindromes[len(palindromes)-1])
+        print("xor=%d" % xor)
 
 # print the list of palindromes
 print(palindromes)
