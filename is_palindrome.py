b2dabd5c1cd61cc1c136edad27dac600556ff409
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-11-04 17:44:14 -0400

    is_palindrome.py: ip_palindrome.py <int> <int>

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,25 +1,71 @@
 #!/usr/bin/env python3
+import sys
 
 # define a function to check if a number is a palindrome
 def is_palindrome(n):
     # convert the number to a string
     s = str(n)
-    
     # check if the string is the same forwards and backwards
     return s == s[::-1]
 
-# define the start and end of the range
-start = 1
-end = 1000001
+def loop(lb, ub):
 
-# create an empty list to store the palindromes
-palindromes = []
+    # define the start and end of the range
+    start = int(lb)
+    end = int(ub)
 
-# loop through the range of numbers
-for n in range(start, end+1):
-    # if the number is a palindrome, add it to the list
-    if is_palindrome(n):
-        palindromes.append(n)
+    # create an empty list to store the palindromes
+    palindromes = []
 
-# print the list of palindromes
-print(palindromes)
+    # loop through the range of numbers
+    for n in range(start, end+1):
+        # if the number is a palindrome, add it to the list
+        if is_palindrome(n):
+            palindromes.append(n)
+
+    # print the list of palindromes
+    if len(palindromes) > 0:
+        print(palindromes)
+
+if (args_count := len(sys.argv)) == 3:
+
+    if int(sys.argv[1]) > int(sys.argv[2]):
+        ## print("int(sys.argv[1]) > int(sys.argv[2])");
+        lower_bound = int(sys.argv[2]);
+        upper_bound = int(sys.argv[1]);
+        loop(lower_bound, upper_bound)
+
+    if int(sys.argv[1]) < int(sys.argv[2]) or int(sys.argv[1]) == int(sys.argv[2]):
+        ## print("int(sys.argv[1]) < int(sys.argv[2])");
+        lower_bound = int(sys.argv[1]);
+        upper_bound = int(sys.argv[2]);
+        loop(lower_bound, upper_bound)
+
+elif args_count == 2:
+
+    if int(sys.argv[1]) > int(sys.argv[2]):
+        print("int(sys.argv[1]) > int(sys.argv[2])");
+        lower_bound = int(0);
+        upper_bound = int(sys.argv[2]);
+        loop(lower_bound, upper_bound)
+
+    if int(sys.argv[1]) < int(sys.argv[2]):
+        print("int(sys.argv[1]) < int(sys.argv[2])");
+        lower_bound = int(0);
+        upper_bound = int(sys.argv[1]);
+        loop(lower_bound, upper_bound)
+
+
+elif args_count > 2:
+
+    print("Usage:\n")
+    print("is_palindrome <int>\n")
+    print("is_palindrome <int> <int>\n")
+    raise SystemExit(0)
+
+elif args_count < 2:
+
+    print("Usage:\n")
+    print("is_palindrome <int>\n")
+    print("is_palindrome <int> <int>\n")
+    raise SystemExit(0)
